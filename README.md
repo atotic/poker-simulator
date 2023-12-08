52142f8817caf6a7b681bcd808005d6895c6517c
Aleks Totic <atotic@gmail.com>
2023-12-07 21:35:39 -0800

    Service worker v1 done

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # poker-simulator
 
 CURRENT
-Use http-server
-https://www.npmjs.com/package/http-server
 
 TODO
+	- serviceWorker test case
+	-	cacheing serviceWorker
 	- installable as an app
 	  - https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Making_PWAs_installable
 		- run server as https: done, but with incorrect certificate authority
@@ -24,7 +24,7 @@ TODO
 		- Lighting Web Components https://lwc.dev/. Noah Lawson
 	
 DONE
-  12/Dec/23 button that circles through suits for mobile?
+  2/Dec/23 button that circles through suits for mobile?
 	- suit popup
   - toolbar happiness: hilo button, sync hole cards with hilo
 	- click on deck to serve cards
@@ -33,5 +33,9 @@ DONE
 	- drag-n-drop: should enlarge the card, give it shadow for that drag look
 	- drag n drop: secret was to cancel drag by adding not passive event listener
 	- view on iPhone http://192.168.1.46:8080/poker.html
+	- Use http-server https://www.npmjs.com/package/http-server
 	- poker engine works
   - drag n drop
+
+	References
+	- manifest.json https://w3c.github.io/manifest/#display-member
--- serviceWorker.js
+++ serviceWorker.js
@@ -28,12 +28,19 @@ ServiceWorkerGlobalScope JavaScript scope service workers run in
   clients: clients associated with this scope.
   registration: 
   all events ServiceWorker responds to are fired here: install, activate, message
-Cache
+
+  caches is a CacheStorage global, but not defined in spec on GlobalScope?
+
 CacheStorage
+  open()
+  keys() returns a list of all cache keys
+Cache
+
 Client
 
 References:
 https://whatwebcando.today/articles/handling-service-worker-updates/
+https://felixgerschau.com/how-to-communicate-with-service-workers/
 
 My first service worker project
 https://web.dev/learn/pwa/service-workers/
@@ -56,28 +63,107 @@ My design:
 - simplest design would not cache anything, but then it would not work offline.
 - 
 */
+'use strict'
+
+let SW_LOG = "";
+
+let CACHE_NAME = "v1";
+let PRE_CACHED_RESOURCES = ["/", "index.html", "playing_card.css", "pokerEngine.js"];
+
+async function precacheResources() {
+  const cache = await caches.open(CACHE_NAME);
+  // Cache all static resources.
+  cache.addAll(PRE_CACHED_RESOURCES);
+}
+
 self.addEventListener("install", ev => {
-  let OLD_CACHES=[];
-  let CACHE_NAME="";
-  let ASSETS=['index.html', 'playing_card.css', 'pokerEngine.js'];
   console.log("Service worker install in progress");
   self.skipWaiting(); // activates new service worker immediately
-
-  ev.waitUntil( _ => {
-    clients.claim();
-    // TODO populate cache
-    return Promise.resolve();
-  });
+  ev.waitUntil(precacheResources());
 });
 
 self.addEventListener("activate", ev => {
   // clear out stale caches here
   console.log("Service worker activated");
 });
+
 self.addEventListener("message", ev => {
-  console.log("SW: message", ev);
+  if (!ev.data)
+    return;
+  console.log("sw message", ev.data.type);
+  switch (ev.data.type) {
+    case "POKER_TEST_GETLOG": {
+      ev.source.postMessage({
+        type: "POKER_TEST_GETLOG_REPLY",
+        log: SW_LOG
+      });
+    }
+    break;
+    case 'POKER_TEST_LISTCACHE': {
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.keys())
+        .then((requests) => {
+          let cacheList = [];
+          for (let r of requests)
+            cacheList.push(r.url.replace("http://", ""));
+          ev.source.postMessage({
+            type: "POKER_TEST_LISTCACHE_REPLY",
+            cacheList: cacheList.join("\n")
+          });
+        });
+    }
+    break;
+    case 'POKER_CLEAR_CACHE': {
+      let deleteCount = 0;
+      caches.keys()
+      .then( keys => {
+        Promise.all(
+          keys.map( key => {
+            console.log("sw: deleting cache ", key);
+            deleteCount++;
+            return caches.delete(key);
+          })
+        )
+      })
+      .then( 
+        result => {
+          ev.source.postMessage({
+            type: "POKER_CLEAR_CACHE_REPLY",
+            result: result
+          });
+        },
+        error => {
+          ev.source.postMessage({
+            type: "POKER_CLEAR_CACHE_REPLY",
+            error: error
+          });
+        }
+      )
+    }
+    break;
+    default:
+      console.error("Unexpected sw message", ev.data.type);
+  }
 });
+
+
 self.addEventListener("fetch", ev => {
-  // https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent
-  return;
+  // https://developer.chrome.com/docs/workbox/caching-strategies-overview
+  // Network-first, copied from google
+  if (ev.request.mode === 'navigate') {
+    // Open the cache
+    ev.respondWith(caches.open(CACHE_NAME).then(async (cache) => {
+      // Go to the network first
+      return fetch(ev.request.url).then((fetchedResponse) => {
+        if (ev.request.url.match("test/") == null) // Do not put test files into the cache
+          cache.put(ev.request, fetchedResponse.clone());
+        return fetchedResponse;
+      }).catch(() => {
+        // If the network is unavailable, get
+        return cache.match(ev.request.url);
+      });
+    }));
+  } else {
+    return;
+  }
 });
