4424850b3d907e61c44d6cc2d8a953fc2073dbdf
Aleks Totic <atotic@gmail.com>
2024-02-05 12:52:53 -0800

    ServiceWorker bug fixes. Starting to get a feel for async

---
+++ debugSW.js
@@ -0,0 +1,134 @@
+/* 
+Service Worker context is:
+ServiceWorkerGlobalScope: clients
+WindowOrWorkerGlobalScope: caches
+*/
+'use strict'
+
+let CACHE_NAME = "DEBUG_CACHE";
+
+// Logging/messaging utilities
+let timestampFormat =  Intl.DateTimeFormat('en-us', {
+  hour12: false, minute: "2-digit", second: "2-digit", fractionalSecondDigits: 3});
+let oldMessages = [];
+let debug = true;
+
+let RANDOM_PREFIX = String.fromCharCode(Math.round(65+Math.random()*26));
+
+function messageClient(msg) {
+  if (debug) console.log(msg);
+  let tsMessage = `${RANDOM_PREFIX} ${timestampFormat.format(new Date())} ${msg}`;
+  clients.matchAll({includeUncontrolled: true})
+    .then(myClients => {
+      if (myClients.length == 0) { // save messages for later
+        oldMessages.push(tsMessage);
+      } else {
+        while (oldMessages.length > 0) {
+          for (let c of myClients)
+            c.postMessage(oldMessages.shift());
+        }
+        for (let c of myClients)
+          c.postMessage(tsMessage);
+      }
+    })
+    .catch(err => { //
+      console.error("Error in messageClient", err);
+    })
+}
+
+// SW events
+self.addEventListener("install", ev => {
+  messageClient("sw install");
+  // deleteOldCaches()
+  self.skipWaiting(); // activates new service worker immediately
+  // ev.waitUntil(precacheResources());
+});
+
+self.addEventListener("activate", ev => {
+  messageClient("sw activate");
+  clients.claim();
+    // .then(deleteOldCaches);
+});
+
+self.addEventListener("fetch", ev => {
+  messageClient(`sw fetch ${ev.request.url}`);
+  ev.respondWith(
+    (async () => {
+      let cache = await caches.open(CACHE_NAME);
+      let response = await cache.match(ev.request.url);
+      if (response) {
+        messageClient(`sw fetch success ${ev.request.url}`);
+        return response;
+      }
+      messageClient(`sw fetch miss ${ev.request.url}`);
+      response = await fetch(ev.request);
+      if (response.ok) {
+        messageClient(`sw fetch storing ${ev.request.url}`);
+        cache.put(ev.request.url, response.clone());
+        messageClient(`sw fetch remote success ${ev.request.url}`);
+        return response;
+      }
+      messageClient(`sw fetch remote failure ${ev.request.url}`);
+      return response;
+    })()
+  );
+});
+
+// Message handlers
+function listCache() {
+  caches.open(CACHE_NAME)
+    .then(cache => cache.keys())
+    .then(requests => {
+      let listMsg = `list cache ${CACHE_NAME}<br>`;
+      if (requests.length == 0)
+        listMsg += "empty!";
+      for (let r of requests) {
+        listMsg += `${r.url}<br>`;
+      }
+      messageClient(listMsg);
+    });
+}
+
+function clearCache() {
+  caches.delete(CACHE_NAME)
+    .then( wasDeleted => {
+      if (wasDeleted)
+        messageClient(`${CACHE_NAME} deleted`);
+      else
+        messageClient(`${CACHE_NAME} not found`);
+    });
+}
+
+function addToCache(path) {
+  caches.open(CACHE_NAME)
+    .then(cache => {
+      cache.add(path);
+    })
+    .then( _ => {
+      messageClient(`success adding ${path}`);
+    })
+    .catch( err => {
+      messageClient(`error adding ${path}: ${err}`);
+    })
+}
+
+// Message events are only used for testing
+self.addEventListener("message", ev => {
+  // if (debug)
+  //   console.log(ev.data);
+  if (!ev.data) {
+    messageClient("got message, no data");
+    return;
+  }
+  if (ev.data.type == "listCache") {
+    listCache();
+  } else if (ev.data.type == "clearCache") {
+    clearCache();
+  } else if (ev.data.type == "addToCache") {
+    addToCache(ev.data.path);
+  } else {
+    messageClient(`unknown message ${ev.data}`);
+  }
+  if (!ev.data)
+    return;
+});
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,64 +1,21 @@
-/*
-Learning ServiceWorker API
-
-ServiceWorker is a script that runs in a background.
-Lifecycle of a service worker:
-download: 
-install: create a cache, download assets. Call skipWaiting for immediate upgrade.
-activate: clean up old caches
-
-ServiceWorker does not become active by default until all old clients are closed.
-Override this with  ServiceWorkerGlobalScope.skipWaiting(), Clients.claim().
-
-API used by the web page:
-
-navigator.serviceWorker is a ServiceWorkerContainer
-ServiceWorkerContainer 
-  register(url) => ServiceWorkerRegistration
-  
-ServiceWorkerRegistration
-  active: currently active worker
-  installing: worker currently installing
-  waiting: installed worker, waiting to become active
-  updatefound event: fires when new service worker enters installing state
-
-APIs used by ServiceWorker
-
-ServiceWorkerGlobalScope JavaScript scope service workers run in
-  clients: clients associated with this scope.
-  registration: 
-  all events ServiceWorker responds to are fired here: install, activate, message
-
-  caches is a CacheStorage global, but not defined in spec on GlobalScope?
-
-CacheStorage
-  open()
-  keys() returns a list of all cache keys
-Cache
-
-Client
-
-References:
-https://whatwebcando.today/articles/handling-service-worker-updates/
-https://felixgerschau.com/how-to-communicate-with-service-workers/
-
-My first service worker project
-https://web.dev/learn/pwa/service-workers/
-Clearing out buggy service workers: https://developer.chrome.com/docs/workbox/remove-buggy-service-workers/
-- i might need this. Creates a null worker.
-https://whatwebcando.today/articles/handling-service-worker-updates/
-
-*/
 /* 
- ServiceWorker strategy:
- - precache resources, wipe out any old caches, skipWaiting
- - claim all clients on activation
+ ServiceWorker simple strategy:
+ - delete old cache on install, precache resources
+ - fetch: cache first, then network. Store network responses in cache
 */
 'use strict'
 
-let PRE_CACHED_RESOURCES = ["/", "index.html", "playing_card.css", "pokerEngine.js", "favicon.ico", "icons/512.png"];
-let SW_VERSION = `PSW_${Math.floor(Math.random() * 10000000)}`;
-let CACHE_NAME = 'v2';
+let CACHE_NAME = 'v1';
+let SW_VERSION = `SW_${String.fromCharCode(Math.round(65+Math.random()*26))} ${CACHE_NAME}`;
+
+let PRE_CACHED_RESOURCES = [
+  "./", 
+  "index.html", 
+  "playing_card.css", 
+  "pokerEngine.js", 
+  "favicon.ico", 
+  "icons/512.png"
+];
 
 async function precacheResources() {
   await caches.delete(CACHE_NAME);  // Brute force, just deletes previous cache that might be live.
@@ -68,114 +25,88 @@ async function precacheResources() {
 }
 
 async function deleteOldCaches() {
-  caches.keys()
-  .then(keys => {
-    Promise.all(
-      keys.map(key => {
-        if (key != CACHE_NAME)
-          return caches.delete(key);
-      })
-    );
-  });
+  let keys = await caches.keys();
+  return Promise.all(
+    keys.map(key => {
+      if (key != CACHE_NAME)
+        return caches.delete(key);
+    })
+  );
 }
 
-self.addEventListener("install", ev => {
-  console.log("Service worker install in progress");
+let NOT_CACHED = ["/manifest.json"];
+function shouldCache(path) {
+  let url = new URL(path);
+  if (url.pathname.startsWith("/test"))
+    return false;
+  for (let path of NOT_CACHED)
+    if (url.pathname == path)
+    return false;
+  return true;
+}
+
+self.addEventListener("install", async (ev) => {
+  // console.log("Service worker install in progress");
   self.skipWaiting(); // activates new service worker immediately
   ev.waitUntil(precacheResources());
 });
 
-self.addEventListener("activate", ev => {
-  clients.claim()
-    .then(deleteOldCaches);
+self.addEventListener("activate", async ev => {
+  await clients.claim();
+  await deleteOldCaches();
   console.log(`Service worker  ${CACHE_NAME} ${SW_VERSION} activated`);
 });
 
-let NOT_CACHED = ["/manifest.json"];
-
 self.addEventListener("fetch", ev => {
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview
+  // Stragegy: cache first; store network responses in cache
   ev.respondWith(
-    caches.open(CACHE_NAME)
-      .then(async cache => {
-        return cache.match(ev.request.url);
-      })
-      .then(async response => {
-        if (response)
-          return response;
-        let url = new URL(ev.request.url);
-        if (!url.pathname.startsWith("/test")) {
-          let should_be_cached = true;
-          for (let path of NOT_CACHED) {
-            if (url.pathname == path) {
-              should_be_cached = false;
-              break;
-            }
-          }
-          if (should_be_cached)
-            console.error(`Fetching in service worker ${ev.request.url}`);
-        }
-        return fetch(ev.request);
-      })
+    (async () => {
+      let cache = await caches.open(CACHE_NAME);
+      let response = await cache.match(ev.request.url);
+      if (response)
+        return response;
+      response = await fetch(ev.request);
+      if (response.ok && shouldCache(ev.request.url)) {
+        console.log(`sw: storing network response ${ev.request.url}`);
+        cache.put(ev.request.url, response.clone());
+      }
+      return response;
+    })()
   );
 });
 
-// Message events are only used for testing
-self.addEventListener("message", ev => {
+// TESTING ONLY
+self.addEventListener("message", async ev => {
   if (!ev.data)
     return;
   console.log("sw message", ev.data.type);
-  switch (ev.data.type) {
-    case "POKER_TEST_GETVERSION": {
-      ev.source.postMessage({
-        type: "POKER_TEST_GETVERSION_REPLY",
-        version: SW_VERSION
-      });
+  if (ev.data.type == "POKER_TEST_GETVERSION") {
+    ev.source.postMessage({
+      type: "POKER_TEST_GETVERSION_REPLY",
+      version: SW_VERSION
+    });
+  } else if (ev.data.type == 'POKER_TEST_LISTCACHE') {
+    let cache = await caches.open(CACHE_NAME);
+    let requests = await cache.keys();
+    let cacheList = requests.map( r => r.url.replace("http://", ""));
+    ev.source.postMessage({
+      type: "POKER_TEST_LISTCACHE_REPLY",
+      cacheList: cacheList.join("\n")
+    });
+  } else if (ev.data.type == 'POKER_CLEAR_CACHE') {
+    let deleteCount = 0;
+    let keys = await caches.keys();
+    try {
+      let deletions = await Promise.all(
+        keys.map( key => {
+          console.log("sw: deleting cache ", key);
+          deleteCount++;
+          return caches.delete(key);
+        }));
+      ev.source.postMessage({type: "POKER_CLEAR_CACHE_REPLY", result: deletions });
+    } catch(err) {
+      ev.source.postMessage({type: "POKER_CLEAR_CACHE_REPLY", error: err });
     }
-    break;
-    case 'POKER_TEST_LISTCACHE': {
-      caches.open(CACHE_NAME)
-        .then(cache => cache.keys())
-        .then(requests => {
-          let cacheList = [];
-          for (let r of requests)
-            cacheList.push(r.url.replace("http://", ""));
-          ev.source.postMessage({
-            type: "POKER_TEST_LISTCACHE_REPLY",
-            cacheList: cacheList.join("\n")
-          });
-        });
-    }
-    break;
-    case 'POKER_CLEAR_CACHE': {
-      let deleteCount = 0;
-      caches.keys()
-      .then( keys => {
-        Promise.all(
-          keys.map( key => {
-            console.log("sw: deleting cache ", key);
-            deleteCount++;
-            return caches.delete(key);
-          })
-        )
-      })
-      .then( 
-        result => {
-          ev.source.postMessage({
-            type: "POKER_CLEAR_CACHE_REPLY",
-            result: result
-          });
-        },
-        error => {
-          ev.source.postMessage({
-            type: "POKER_CLEAR_CACHE_REPLY",
-            error: error
-          });
-        }
-      )
-    }
-    break;
-    default:
-      console.error("Unexpected sw message", ev.data.type);
-  }
+  } else
+    console.error("Unexpected sw message", ev.data.type);
 });
